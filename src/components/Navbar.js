900b853c6087c840c7ebbce03a7598565ea6faa0
Szabina Kardos <82178685+sz-kardos@users.noreply.github.com>
2024-02-04 19:55:43 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -110,7 +110,7 @@ const Navbar = () => {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
-                            href='https://www.linkedin.com/in/szabina-kardos-606b3120b/'
+                            href='/'
                         >
                             Linkedin <FaLinkedin size={30} />
                         </a>
@@ -118,7 +118,7 @@ const Navbar = () => {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
-                            href='https://github.com/sz-kardos'
+                            href='/'
                         >
                             Github <FaGithub size={30} />
                         </a>
@@ -126,7 +126,7 @@ const Navbar = () => {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
-                            href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCKCDBwDcQhwbXLRRCkNjSnWvGKqJNqvVSCbltPzQHXLmzxLlCdttVnZFzSMJzHRPwMDDxPL'
+                            href='/'
                         >
                             Email <HiOutlineMail size={30} />
                         </a>
